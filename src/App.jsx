8693e2fb14caa9d3c413b1187960f0364e46e821
edwardmuthie <edward.g@turing.com>
2024-10-17 16:55:50 +0300

    203529_modelB

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const Homepage = () => {
+function App() {
   return (
     <div>
-      <h1>Welcome to the Homepage!</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Homepage</Link>
+          </li>
+          <li>
+            <Link to="/chat">Chat</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Rest of your app */}
     </div>
   );
-};
-
-const Chat = () => {
-  return (
-    <div>
-      <h1>Welcome to the Chat!</h1>
-    </div>
-  );
-};
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Homepage</Link>
-            </li>
-            <li>
-              <Link to="/chat">Chat</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/chat" element={<Chat />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
-};
+}
 
 export default App;
